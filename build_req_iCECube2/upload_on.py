1e2ecf0f903c356a72f784b3afaef33ccf444e8b
develone <develone@sbcglobal.net>
2016-10-24 18:02:32 -0600

    adding iCECube2  required build

---
+++ build_req_iCECube2/upload_on.py
@@ -0,0 +1,15 @@
+import serial
+file = open("led_on.bin","wb")
+file.write("\xde\x02\x00\x00\x00\x80\x04\xca\x00\x00\x00\xff")
+file.close
+file = open("led_on.bin", "rb")
+data = file.read(12)
+ser = serial.Serial ("/dev/ttyAMA0")    
+ser.baudrate = 115200                      
+
+#print data
+ser.write(data) 
+reply = ser.read(12) 
+print "this is the reply", reply 
+file.close
+
